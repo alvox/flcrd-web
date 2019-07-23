fe26a740db16fde25abd6dc553db7d46311ade1b
alexander <al@alvox.net>
2019-07-23 09:28:54 +0300

    Add models for deck and flashcard.

---
+++ pkg/models/models.go
@@ -0,0 +1,19 @@
+package models
+
+import "time"
+
+type Flashcard struct {
+	ID      string    `json:"id"`
+	DeckID  string    `json:"deck_id"`
+	Front   string    `json:"front"`
+	Rear    string    `json:"rear"`
+	Created time.Time `json:"created"`
+}
+
+type Deck struct {
+	ID          string      `json:"id"`
+	Name        string      `json:"name"`
+	Description string      `json:"description"`
+	Cards       []Flashcard `json:"cards"`
+	Created     time.Time   `json:"created"`
+}
