4a5963712712af410c479ddfb31452ff0d48ce2f
alvox <al@alvox.net>
2019-10-02 18:47:34 -0400

    Use github docker registry.

--- env/build.sh
+++ env/build.sh
@@ -2,6 +2,6 @@
 
 rm -f app
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o app ../cmd/web/*
-docker build -t registry.gitlab.com/alvox-env/registry/flcrd-api:latest .
-docker push registry.gitlab.com/alvox-env/registry/flcrd-api:latest
+docker build -t docker.pkg.github.com/alvox/flcrd-web/flcrd-api:latest .
+docker push docker.pkg.github.com/alvox/flcrd-web/flcrd-api:latest
 rm -f app
