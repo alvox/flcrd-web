57bf53b6b94a22dea845f013658389dcc47c7f86
alvox <al@alvox.net>
2019-10-04 17:26:23 -0400

    Move Dockerfile to the top level directory.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -f app
-CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o app ../cmd/web/*
+CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o app cmd/web/*
 docker build -t docker.pkg.github.com/alvox/flcrd-web/flcrd-api:latest .
 docker push docker.pkg.github.com/alvox/flcrd-web/flcrd-api:latest
 rm -f app
